89c68780e8d312c1cac2488c4e83766bddcc16e4
Nicolas Giovanella <nicolasfgiovanella539@gmail.com>
2023-07-07 06:48:58 -0300

    add functions para salvar tarefas no storage do navegador, e para cria um <p> cada vez que cllicar no botar de enviar

--- script.js
+++ script.js
@@ -3,26 +3,50 @@ const tarefa = document.querySelector(".tarefas");
 const textoTarefa = document.querySelector(".input-tarefa");
 
 function criaP(){ //clicar no botão concluido
-   // const elemento =e.target;
     const p =document.createElement("p"); //criar elemento HTML
-    p.setAttribute("class", "apagar")
+    p.setAttribute("class", "tarefa")
     p.setAttribute("contenteditable", "false");
-    tarefa.appendChild(p); //dizendo que o li é filho de tarefa(ul)
-    //p.innerHTML = "teste";
+    tarefa.appendChild(p); 
     return p;
 }
 
-function enviaTarefa(){
+function enviaTarefa(textoInput){
     const p = criaP();
-    p.innerHTML = textoTarefa.value;
+    p.innerHTML = textoInput;
 }
 
 button.addEventListener("click", function(e){
-    enviaTarefa();
+    enviaTarefa(textoTarefa.value);
     limpaInput();
+    salvarStorage();
 })
 
 function limpaInput(){
     textoTarefa.value = "";
     textoTarefa.focus();
 }
+
+function salvarStorage(){
+    const pTarefas= tarefa.querySelectorAll("p");
+    const listaTarefa = [];
+
+    for(let valor of pTarefas){
+        let tarefaTexto = valor.innerText;
+        tarefaTexto = tarefaTexto.replace("apagar", "").trim();//replace para apagar palavra indicada, e trim para apagar espaço vazio
+        console.log(tarefaTexto);
+        listaTarefa.push(tarefaTexto);//adicionar ao final do array
+    }
+    const tarefasJSON = JSON.stringify(listaTarefa);
+    localStorage.setItem("tarefas", tarefasJSON); 
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas =localStorage.getItem("tarefas");
+    const listaTarefa = JSON.parse(tarefas);
+
+    for (let tarefa of listaTarefa){
+        enviaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas();
